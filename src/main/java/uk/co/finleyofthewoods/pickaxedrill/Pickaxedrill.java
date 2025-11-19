a61b751452423017be52275ef420c8d1e5e8554e
Finley Cordell <finley.of.the.woods@gmail.com>
2025-11-19 15:08:31 +0000

    first commit

---
+++ src/main/java/uk/co/finleyofthewoods/pickaxedrill/Pickaxedrill.java
@@ -0,0 +1,10 @@
+package uk.co.finleyofthewoods.pickaxedrill;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pickaxedrill implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/uk/co/finleyofthewoods/pickaxedrill/client/PickaxedrillDataGenerator.java
@@ -0,0 +1,12 @@
+package uk.co.finleyofthewoods.pickaxedrill.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PickaxedrillDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
